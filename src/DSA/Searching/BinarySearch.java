02b87a5ac6b6e531de352213aa821d8d327de3f6
SuryanshuTomar <suryanshutomar16@gmail.com>
2021-12-19 11:57:00 +0530

    Linear and Binary Search Added.

---
+++ src/DSA/Searching/BinarySearch.java
@@ -0,0 +1,30 @@
+package DSA.Searching;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int [] arr = {-20, 3, 5, 8, 12, 54,234, 350};
+        int target = 350;
+        int result = binarySearch(arr, target);
+        System.out.println(result);
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
