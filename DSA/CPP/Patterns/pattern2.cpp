1f8de06ba8dba90fa20a7430c240fd0edf08144c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-21 21:41:38 +0530

    Updated pattern 2

--- DSA/CPP/Patterns/pattern2.cpp
+++ DSA/CPP/Patterns/pattern2.cpp
@@ -6,11 +6,24 @@ int main(){
    std::cout << "Enter the Number of Rows you want for the pattern : ";
    std::cin >> rowNum;
 
+   // For Loop Approach
    for(int row = 1; row <= rowNum ; row++){
       for(int col = 0; col < rowNum ; col++){
          std::cout << row <<  " ";
       }
       std::cout << std::endl;
+   }
+
+   // While Loop Approach
+   int row {1};
+   while(row <= rowNum){
+   int col {0};
+      while(col < rowNum){
+         std::cout << row << " ";
+         col++;
+      }
+      std::cout << std::endl;
+      row++;
    }
 
 }
