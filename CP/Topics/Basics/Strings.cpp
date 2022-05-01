9d0a711478089b6e6264d78a158ed0835f271cb6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-01 13:50:06 +0530

    String Basics

---
+++ CP/Topics/Basics/Strings.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+int main(){
+   // 1. Taking Input
+   // std::string str1 = "Hello";
+   // std::cout << str1 << std::endl;
+
+   // std::string str2;
+   // std::cout << "Enter String Input : " << std::endl;
+   // std::cin >> str2;
+   // std::cout << str2 << std::endl;
+
+   // 2. String Concatination
+   // std::string str3, str4;
+   // str3 = "Hello ";
+   // str4 = "World ";
+   // std::cout << str3 + str4 << std::endl;
+
+   // 3. String Compare
+   // if(str1 == str2){
+   //    std::cout << "Equal" << std::endl;
+   // }
+   // else{
+   //    std::cout << "Not Equal" << std::endl;
+   // }
+
+   // 4. String character Access
+   // std::string str5 = "DEathSlayer";
+   // str5[1] = 'e';
+   // std::cout << str5 << std::endl;
+
+   // 5. String size and Looping
+   // std::string str6 = "Alex Mercer";
+   // for(int i = 0; i < str6.size(); i++){
+   //    std::cout << str6[i] << std::endl;
+   // }
+
+   // 6. Taking string input with space
+   // std::string str7;
+   // std::cout << "Enter Input : ";
+   // getline(std::cin, str7);
+   // std::cout << str7 << std::endl;
+
+   // 7. Taking string input for multiple Test Cases 
+   int t;
+   std::cin >> t;
+   std::cin.ignore();
+   while(t--){
+      std::string str8;
+      getline(std::cin , str8);
+      std::cout << str8 << std::endl;
+   } 
+
+   // 3
+   // abc def
+   // ghi jkl
+   // mno pqr
+
+   return 0;
+} 
