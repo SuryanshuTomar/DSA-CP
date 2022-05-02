6462d9d2a1aa2a08fef44ac8f84eefb81c70ebc3
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-02 16:53:59 +0530

    Q-4 City Tour

---
+++ CP/Challenges/Challenge-1/City_Tour.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerearth.com/problem/algorithm/city-tour/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int test;
+   cin >> test;
+
+   while(test--){
+      long long int dist;
+      cin >> dist;
+
+      // Distance in 4 quarters
+      cout << 4 * dist << endl;
+   }
+   return 0;
+}
