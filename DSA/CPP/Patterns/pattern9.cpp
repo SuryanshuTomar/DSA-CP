9ec558a9bf9835f41c5e12c0a66095c6e16c787b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-04 11:27:28 +0530

    Pattern - 9

---
+++ DSA/CPP/Patterns/pattern9.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int rowNums {};
+   cout << "Enter the Number of Rows you want for the pattern : ";
+   cin >> rowNums; 
+
+   // For Loop Approach  
+   for(int row = 1; row <= rowNums; row++){
+      for(int col = row; col < 2*row; col++){
+         cout << col << " ";
+      }
+      cout << endl;
+   }
+
+   // While Loop Approach 
+   int row {1};
+   while(row <= rowNums){
+      int col {row};
+      while(col < 2* row){
+         cout << col << " ";
+         col++;
+      }
+      cout << endl;
+      row++;
+   }
+
+   return 0;
+}
