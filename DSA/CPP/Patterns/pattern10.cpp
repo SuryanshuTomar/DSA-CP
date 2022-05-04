d594b3f9f346642e1c0891b84c37b36b27c1b420
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-04 11:32:44 +0530

    Pattern 10

---
+++ DSA/CPP/Patterns/pattern10.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int rowNums {};
+   cout << "Enter the Number of Rows you want for the pattern : ";
+   cin >> rowNums; 
+
+   // For Loop Approach  
+   for(int row = 1; row <= rowNums; row++){
+      for(int col = row; col >= 1; col--){
+         cout << col << " ";
+      }
+      cout << endl;
+   }
+
+   // While Loop Approach
+   int row {1};
+   while (row <= rowNums){
+      int col {row};
+      while (col >= 1) {
+         cout << col << " ";
+         col--;
+      }
+      cout << endl;
+      row++;
+      
+   }
+
+   return 0;
+}
