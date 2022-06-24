78b1f2d6ac95ad330776567496d7a8552b941ef6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-24 09:04:39 +0530

    improved code for sorted and rotated array

--- DSA/CPP/Arrays/SortedAndRotated.cpp
+++ DSA/CPP/Arrays/SortedAndRotated.cpp
@@ -16,22 +16,24 @@ void printVector(vector<int> &vec){
 bool isSortedAndRotated(vector<int> &vec){
    int count = 0;
 
+   // Checking if sorted and rotated
    for(int i = 0; i < vec.size() - 1; i++){
       if(vec[i] > vec[i+1]){
          count++;
       }
-      if(count > 1){
-         return false;
-      }
    }
 
-   if(vec[vec.size() - 1] > vec[0] && count == 1) return false;
+   // Returing false if not sorted and rotated from both ends
+   if(vec[vec.size() - 1] > vec[0]) count++;
 
-   return true;
+   // Returning true if the array is only sorted and if the array is sorted and rotated otherwise false
+   // sorted => count < 1
+   // sorted and rotated => count = 1
+   return count <= 1;
 }
 
 int main(){
-   vector<int> vec = {6, 10, 6};
+   vector<int> vec = {7, 1, 3, 6};
    bool result = isSortedAndRotated(vec);
    cout << result << endl;
 
