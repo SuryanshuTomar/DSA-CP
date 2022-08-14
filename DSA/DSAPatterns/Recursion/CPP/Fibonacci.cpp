134fc05562138546299131e429464ae4450b04fb
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-14 18:25:12 +0530

    Fibonacci Series

---
+++ DSA/DSAPatterns/Recursion/CPP/Fibonacci.cpp
@@ -0,0 +1,20 @@
+// https://www.codingninjas.com/codestudio/problems/nth-fibonacci-number_74156
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int findFibonacci(int n){
+    if(n == 2 || n == 1){
+        return 1;
+    }
+    int fib = findFibonacci(n-1) + findFibonacci(n-2);
+    return fib;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    cout << findFibonacci(n);
+}
---
+++ DSA/DSAPatterns/Recursion/JS/Fibonacci.js
@@ -0,0 +1,15 @@
+// https://www.codingninjas.com/codestudio/problems/nth-fibonacci-number_74156
+
+function findFibonacci(n) {
+	if (n == 2 || n == 1) {
+		return 1;
+	}
+	return findFibonacci(n - 1) + findFibonacci(n - 2);
+}
+
+function main() {
+	let n = 8;
+	console.log(findFibonacci(n));
+}
+
+main();
