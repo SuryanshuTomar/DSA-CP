8fac4d5315c774dbf056a9a30f0af618c69ca595
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-15 11:04:46 +0530

    String Palindrome

---
+++ DSA/DSAPatterns/Recursion/CPP/Palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string &s){
+	// Write your code here.
+   if(s == "") return true;
+   
+   if(s[0] == s[s.size() - 1]){
+      string newStr = s.substr(1, s.size() - 2);
+      return isPalindrome(newStr);
+   } 
+   return false;
+}
+
+bool isPalindrome2(string &s, int l, int r){
+   if(l >= r) return true;
+   if(s[l] != s[r]) return false;
+   return isPalindrome2(s, l+1, r-1);
+}
+
+int main(){
+   string str = "racecar";
+   cout << isPalindrome(str) << endl;
+   cout << isPalindrome2(str, 0, str.size() - 1);
+   return 0;
+}
---
+++ DSA/DSAPatterns/Recursion/JS/Palindrome.js
@@ -0,0 +1,12 @@
+function isPalindrome(str, left, right) {
+	if (left >= right) return true;
+	if (str[left] != str[right]) return false;
+	return isPalindrome(str, left + 1, right - 1);
+}
+
+function main() {
+	let str = "stoots";
+	console.log(isPalindrome(str, 0, str.length - 1));
+}
+
+main();
