4c3ff183e1cb87db2b333bcc970b45544824fbaa
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-19 14:41:07 +0530

    String Subsequence

---
+++ DSA/DSAPatterns/SubsetAndSubsequence/C++/ASCIISubsequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> findASCIISubsequence(vector<string>& result, string processed, string unprocessed, int index){
+   if(index == unprocessed.size()){
+      result.push_back(processed);
+      return result;
+   }
+
+   char ch = unprocessed[index];
+   findASCIISubsequence(result, processed, unprocessed, index + 1);
+   findASCIISubsequence(result, processed + ch, unprocessed, index + 1);
+   findASCIISubsequence(result, processed + to_string(int(ch)), unprocessed, index + 1);
+   return result;
+}
+
+
+int main(){
+   string unprocessed = "ab";
+   string processed = "";
+   vector<string> result;
+   findASCIISubsequence(result, processed, unprocessed, 0);
+   for(auto i: result){
+      cout << i << endl;
+   }
+   cout << endl;
+   return 0;
+}
--- DSA/DSAPatterns/SubsetAndSubsequence/C++/StringSubsequence.cpp
+++ DSA/DSAPatterns/SubsetAndSubsequence/C++/StringSubsequence.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-void findSubsets(vector<string> &result, string processed, string unprocessed, int index){
+void findSubsequence(vector<string> &result, string processed, string unprocessed, int index){
    if(index == unprocessed.size()){
       result.push_back(processed);
       return;
@@ -12,16 +12,16 @@ void findSubsets(vector<string> &result, string processed, string unprocessed, i
 
    char ch = unprocessed[index];
    // Taking the element to add to subset
-   findSubsets(result, processed + ch, unprocessed, index + 1);
+   findSubsequence(result, processed + ch, unprocessed, index + 1);
    // Not taking the element and ignoring it.
-   findSubsets(result, processed, unprocessed, index + 1);
+   findSubsequence(result, processed, unprocessed, index + 1);
 }
 
 int main(){
    string unprocessed = "abc";
    string processed = "";
    vector<string> result;
-   findSubsets(result, processed, unprocessed, 0);
+   findSubsequence(result, processed, unprocessed, 0);
 
    for(auto i: result){
       cout << i << endl;
