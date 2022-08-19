61f9d3b8061a772c97c702ebd63a0eb50ea3e448
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-19 20:36:45 +0530

    Array Subset Iterative

--- DSA/DSAPatterns/SubsetAndSubsequence/C++/ArraySubsets.cpp
+++ DSA/DSAPatterns/SubsetAndSubsequence/C++/ArraySubsets.cpp
@@ -15,11 +15,33 @@ void findSubsets(vector<int> processed, vector<int> unprocessed, vector<vector<i
    processed.push_back(unprocessed[index]);
    findSubsets(processed, unprocessed, result, index + 1);
 }
+
+void findSubsetsIterative(vector<int>& nums, vector<vector<int>>& result){
+   for(auto num : nums){
+       // making a copy of given 2-d vector.
+      vector<vector<int>> subsetTemp = result; // subsetTemp = {{}}
+      for(int i = 0; i < subsetTemp.size(); i++){
+         // adding set[i] element to each subset of subsetTemp.
+         // like adding {1} in 1st iteration(i=0) to {{}} which will give => {{1}}.
+         // or like adding {2} in 2nd iteration to {{},{1}} which will give => {{2},{1,2}}.
+         subsetTemp[i].push_back(num);
+      }
+      for(int i = 0; i < subsetTemp.size(); i++){
+         // now adding modified subsetTemp to original subset -> 
+         //(subsetTemp->{{},{1}} + subsetTemp->{{2}, {1, 2}} => {{},{1},{2},{1,2}}) 
+         result.push_back(subsetTemp[i]);
+      }
+   }  
+}
     
 vector<vector<int>> subsets(vector<int>& nums) {
-   vector<vector<int>> result;
+   vector<vector<int>> result = {{}};
    vector<int> processed;
-   findSubsets(processed, nums, result, 0);
+
+   ///////////////////// Uncomment any one at a time for result : ///////////////
+   // findSubsets(processed, nums, result, 0);
+   // findSubsetsIterative(nums, result);
+
    return result;
 }
 
@@ -27,11 +49,14 @@ vector<vector<int>> subsets(vector<int>& nums) {
 int main(){
    vector<int> vec = {1,2,3};
    vector<vector<int>> result = subsets(vec);
+         cout << "{ ";
    for(auto i: result){
-         cout << endl;
+         cout << "{ ";
       for(auto j: i){
          cout << j << ", ";
       }
+         cout << " }";
    }
+         cout << " }" << endl;
    return 0;
 }
