6d9f6611277aea53de6fbde01f905be9b6a5e1c5
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-20 18:10:48 +0530

    String Permutations with count

--- DSA/DSAPatterns/PermutationsAndCombination/C++/StringPermutation.cpp
+++ DSA/DSAPatterns/PermutationsAndCombination/C++/StringPermutation.cpp
@@ -15,6 +15,20 @@ void findStringPermutations(string processed, string unprocessed, vector<string>
    }
 }
 
+int findStringPermutationsWithCount(string processed, string unprocessed, vector<string>& result){
+   if(unprocessed.empty()){
+      result.push_back(processed);
+      return 1;
+   }
+   int count = 0;
+   char ch = unprocessed[0];
+   for(int i = 0; i <= processed.size(); i++){
+      string firstSubstring = processed.substr(0, i);
+      string lastSubstring = processed.substr(i, processed.length());
+      count += findStringPermutationsWithCount(firstSubstring + ch + lastSubstring , unprocessed.substr(1, unprocessed.length()), result);
+   }
+   return count;
+}
 
 void printVector(vector<string>& result){
    cout << "{ ";
@@ -28,7 +42,9 @@ void printVector(vector<string>& result){
 int main(){
    string str = "abc";
    vector<string> result;
-   findStringPermutations("" ,str, result);
+   // findStringPermutations("" ,str, result);
+   int count = findStringPermutationsWithCount("" ,str, result);
+   cout << "Permutation Count : " << count << endl;
    printVector(result);
    return 0;
 }
