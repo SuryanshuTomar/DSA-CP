952a8031efb0e29e8fd947ad751c0a5b8d85037b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-21 11:11:05 +0530

    Array Permutations Approach 2

--- DSA/DSAPatterns/PermutationsAndCombination/C++/ArrayPermutations.cpp
+++ DSA/DSAPatterns/PermutationsAndCombination/C++/ArrayPermutations.cpp
@@ -1,28 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findPermutations(int index, vector<int> processed, vector<int>& unprocessed, vector<vector<int>>& result){
+// Approach 1 : Using a new vector
+void findPermutations1(int index, vector<int> processed, vector<int>& unprocessed, vector<vector<int>>& result){
    if(index == unprocessed.size()){
       result.push_back(processed);
       return;
    }
-   // Approach 1 : Using a new vector
-   // int num = unprocessed[index];
-   // for(int i = 0; i <= processed.size(); i++){
-   //    vector<int> newP;
-   //    newP.assign(processed.begin(), processed.begin() + i);
-   //    newP.push_back(num);
-   //    newP.insert(newP.end(), processed.begin() + i, processed.end());
-   //    findPermutations(index + 1, newP, unprocessed, result);
-   // }
+   int num = unprocessed[index];
+   for(int i = 0; i <= processed.size(); i++){
+      vector<int> newP;
+      newP.assign(processed.begin(), processed.begin() + i);
+      newP.push_back(num);
+      newP.insert(newP.end(), processed.begin() + i, processed.end());
+      findPermutations1(index + 1, newP, unprocessed, result);
+   }
+}
+// Approach 2 : By Swapping elements (Better Approach)
+void findPermutations2(int index, vector<int> processed, vector<vector<int>>& result){
+   if(index == processed.size()){
+      result.push_back(processed);
+      return;
+   }
 
-   
+for(int i = index; i < processed.size(); i++){
+
+   swap(processed[index], processed[i]);
+   findPermutations2(index + 1, processed, result);
+   // Backtracking for next iteration call
+   swap(processed[index], processed[i]);
+}
 }
    
 vector<vector<int>> permute(vector<int>& nums) {
    vector<vector<int>> result = {};
    vector<int> processed = {};
-   findPermutations(0, processed, nums, result);
+   // findPermutations1(0, processed, nums, result);
+   sort(nums.begin(), nums.end());
+   findPermutations2(0, nums, result);
    return result;
 }
 
@@ -39,7 +54,7 @@ void printNestedVector(vector<vector<int>>& result){
 }
 
 int main(){
-   vector<int> arr = {1, 2, 3};
+   vector<int> arr = {1, 2, 1};
    vector<vector<int>> result = {{}};
    result = permute(arr);
    printNestedVector(result);
