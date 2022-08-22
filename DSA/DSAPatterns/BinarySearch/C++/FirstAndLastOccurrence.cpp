8ffca637346032ee28fc68dad09895d76bf8dbd2
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-22 11:50:44 +0530

    First and Last Occurence with count

---
+++ DSA/DSAPatterns/BinarySearch/C++/FirstAndLastOccurrence.cpp
@@ -0,0 +1,58 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int findFirstOccurrence(vector<int> arr, int target){
+   int start = 0;
+   int end = arr.size() - 1;
+   int targetCurrentIndex = -1;
+
+   while(start <= end){
+   int mid = start + (end - start) / 2;
+      if(arr[mid] == target){
+         targetCurrentIndex = mid;
+         end = mid - 1;
+      }
+      else if (target < arr[mid]){
+         end = mid - 1;
+      }
+      else{
+         start = mid + 1;
+      }
+   }
+   return targetCurrentIndex;
+}
+
+int findLastOccurrence(vector<int> arr, int target){
+   int start = 0;
+   int end = arr.size() - 1;
+   int targetCurrentIndex = -1;
+
+   while(start <= end){
+   int mid = start + (end - start) / 2;
+      if(arr[mid] == target){
+         targetCurrentIndex = mid;
+         start = mid + 1;
+      }
+      else if (target < arr[mid]){
+         end = mid - 1;
+      }
+      else{
+         start = mid + 1;
+      }
+   }
+   return targetCurrentIndex;
+}
+
+int main() {
+    // Write C++ code here
+    vector<int> arr = {1, 2, 3, 10, 10, 10, 10, 12, 12, 14};
+    int target = 10;
+    int firstOccurrence = findFirstOccurrence(arr, target);
+    int lastOccurrence = findLastOccurrence(arr, target);
+   
+    cout << "First : " << firstOccurrence << endl;
+    cout << "Second : " << lastOccurrence << endl;
+    cout << "Count : " << lastOccurrence - firstOccurrence + 1 << endl;
+    return 0;
+}
