66ab5510f5dc5b9b5bc5d0bb22e036db8b73874f
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-23 13:35:45 +0530

    Handled Edge Cases

--- DSA/DSAPatterns/BinarySearch/C++/CeilAndFloopOfNumber.cpp
+++ DSA/DSAPatterns/BinarySearch/C++/CeilAndFloopOfNumber.cpp
@@ -10,6 +10,9 @@ int findCeiling(vector<int>& arr, int target){
    int start = 0;
    int end = arr.size() - 1;
    int result = -1;
+
+   // Edge case
+   if(target > arr[end]) return -1;
    
    while(start <= end){
       int mid = start + (end - start) / 2;
@@ -32,6 +35,9 @@ int findFloor(vector<int>& arr, int target){
    int start = 0;
    int end = arr.size() - 1;
    int result = -1;
+
+   // Edge case
+   if(target < arr[start]) return -1;
    
    while(start <= end){
       int mid = start + (end - start) / 2;
