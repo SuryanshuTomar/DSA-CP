74d4cdcc0880b22543f4061dbeacbf1e1fb75f72
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-23 11:32:21 +0530

    Approach 1 for no of times array is sorted

--- DSA/DSAPatterns/BinarySearch/C++/TimeSortedArrayIsRotated.cpp
+++ DSA/DSAPatterns/BinarySearch/C++/TimeSortedArrayIsRotated.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int findNoOfTimesArrayIsRotated(vector<int>& arr){
-   int start = 0;
+ int start = 0;
    int size = arr.size();
    int end = size - 1;
    int result = -1;
@@ -15,28 +15,28 @@ int findNoOfTimesArrayIsRotated(vector<int>& arr){
       return start;
    }
 
-   while(start <= end){
    int mid = start + (end - start) / 2;
+   while(start <= end){
       // We are taking modulo here in case mid reaches end index then mid + 1 will be out of bound, This modulo will set the next to 0 index instead of out of bound.
       int next = (mid + 1) % size;
       // We are taking modulo here in case mid reaches 0 index then mid - 1 will be negative, This modulo will set the prev to end index instead of out of bound.
       int prev = (mid - 1 + size) % size;
 
-      if(arr[mid] <= arr[prev] && arr[mid] <= arr[next]){
+      if(arr[mid] < arr[prev] && arr[mid] < arr[next]){
          result = mid;
          return result;
       }
-      else if(arr[start] <= arr[mid]){
-         start = mid + 1;
+      else if(arr[start] > arr[prev] && arr[end] > arr[prev]){  
+         mid = prev;
       }else {
-         end = mid - 1;
+         mid = next;
       }
    }
    return result;
 }
 
 int main(){
-   vector<int> arr = {11, 12, 14, 18 ,2, 5, 6, 8};
+   vector<int> arr = {15, 17, 18, 1,  2, 5, 6, 8, 9};
    int noOfRotation = findNoOfTimesArrayIsRotated(arr);
    cout << noOfRotation << endl;
    return 0;
