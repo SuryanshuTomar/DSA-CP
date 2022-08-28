3e865dae12ce749205e4a8e4e753fd5c610de3b6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-28 18:29:05 +0530

    Circular Linked List

--- DSA/JavaScript/LinkedList/CircularLinkedList.js
+++ DSA/JavaScript/LinkedList/CircularLinkedList.js
@@ -17,15 +17,90 @@ class CircularLinkedList {
 		if (this.isEmpty()) {
 			this.head = node;
 			this.tail = node;
-			// this.tail.next = this.head;
+			this.tail.next = this.head;
 			this.size++;
 			return;
 		}
+		this.tail.next = node;
 		node.next = this.head;
-		this.tail.next = this.head;
 		this.head = node;
 		this.size++;
-		console.log("Next : ", this.tail.next);
+	}
+
+	append(value) {
+		if (this.isEmpty()) {
+			this.prepend(value);
+			return;
+		}
+		const node = new Node(value);
+		this.tail.next = node;
+		node.next = this.head;
+		this.tail = node;
+		this.size++;
+	}
+
+	insert(index, value) {
+		if (index > 0 && this.isEmpty()) {
+			console.log("Index does not exists");
+			return;
+		}
+		if (index == 0) {
+			this.prepend(value);
+			return;
+		}
+		if (index == this.size - 1) {
+			this.append(value);
+			return;
+		}
+		let node = new Node(value);
+		let prev = this.getNode(index - 1);
+		node.next = prev.next;
+		prev.next = node;
+		this.size++;
+	}
+
+	deleteByIndex(index) {
+		if (index < 0 || index > this.size - 1) {
+			console.log("Index does not exists");
+			return -1;
+		}
+		let deletedValue;
+		if (index == 0) {
+			deletedValue = this.head.value;
+			if (this.size == 1) {
+				this.head = null;
+				this.tail = null;
+			} else {
+				this.head = this.head.next;
+				this.tail.next = this.head;
+			}
+			this.size--;
+			return deletedValue;
+		}
+		let prev = this.getNode(index - 1);
+		if (index == this.size - 1) {
+			deletedValue = this.tail.value;
+			this.tail = prev;
+			prev.next = this.head;
+			this.size--;
+			return deletedValue;
+		}
+		let temp = prev.next;
+		prev.next = temp.next;
+		deletedValue = temp.value;
+		temp.next = null;
+		this.size--;
+		return deletedValue;
+	}
+
+	getNode(index) {
+		let temp = this.head;
+		let i = 0;
+		while (i < index) {
+			temp = temp.next;
+			i++;
+		}
+		return temp;
 	}
 
 	isEmpty() {
@@ -45,7 +120,7 @@ class CircularLinkedList {
 			process.stdout.write(`${temp.value}`);
 			process.stdout.write(` -> `);
 			temp = temp.next;
-		} while (temp != this.tail);
+		} while (temp != this.head);
 		process.stdout.write(`Tail -> Head `);
 		console.log();
 	}
@@ -56,12 +131,35 @@ function main() {
 	console.log(list.isEmpty());
 	console.log(list.getSize());
 	list.print();
+
 	list.prepend(30);
 	list.prepend(20);
-	// list.prepend(10);
+	list.prepend(10);
+	list.print();
 	console.log(list.isEmpty());
 	console.log(list.getSize());
+
+	list.append(40);
+	list.append(50);
+	list.append(60);
 	list.print();
+	console.log(list.isEmpty());
+	console.log(list.getSize());
+
+	list.insert(1, 70);
+	list.insert(2, 80);
+	list.insert(3, 90);
+	list.print();
+	console.log(list.isEmpty());
+	console.log(list.getSize());
+
+	list.print();
+	list.deleteByIndex(1);
+	list.deleteByIndex(2);
+	list.deleteByIndex(3);
+	list.print();
+	console.log(list.isEmpty());
+	console.log(list.getSize());
 }
 
 main();
