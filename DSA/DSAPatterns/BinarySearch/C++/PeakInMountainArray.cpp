a3436749f822fdf437c653e203876e56c39c3347
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-30 10:04:54 +0530

    Peak in Mountain Array.

--- DSA/DSAPatterns/BinarySearch/C++/PeakInMountainArray.cpp
+++ DSA/DSAPatterns/BinarySearch/C++/PeakInMountainArray.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/peak-index-in-a-mountain-array/
+// https://leetcode.com/problems/find-peak-element/
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -25,7 +27,7 @@ int findPeak(vector<int>& arr){
     return result;
 }
 
-// approach 2
+// approach 2 - better approach
 int findPeak2(vector<int>& arr){
       int start = 0;
     int end = arr.size() - 1;
@@ -33,19 +35,24 @@ int findPeak2(vector<int>& arr){
     while(start < end){
             int mid = start + (end - start) / 2;
             if(arr[mid] > arr[mid + 1]){
+                // This means we are in the decreasing part of the array.
+                // Doing end = mid and not mid - 1, Because we don't if mid - 1 > mid.
                 end = mid;
             }
             else{
+                // This means we are in the increasing part of the array.
+                // Here we have checked that mid is less than mid + 1, so start = mid + 1
                 start = mid + 1;
             }
         }
+        // In the end, start == end, start will be equal to end because of the above two checks, Therefore, start and end will point to the peak element in the array. So, we can return either. 
     return start;
 }
 
 int main() {
-    vector<int> arr = {3, 5, 7, 9, 10, 90, 100};
-    int result = findPeak(arr);
-   //  int result = findPeak2(arr);
+    vector<int> arr = {3, 5, 7, 9, 10, 90, 100, 80, 65, 40};
+    // int result = findPeak(arr);
+    int result = findPeak2(arr);
     cout << result << endl;
     return 0;
 }
