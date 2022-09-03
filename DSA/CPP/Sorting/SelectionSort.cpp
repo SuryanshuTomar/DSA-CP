8a21c8de698ac25a7a1cdda9b0ecdc98bef1b9ff
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-03 22:30:28 +0530

    selection sort

---
+++ DSA/CPP/Sorting/SelectionSort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVector(vector<int> &result) {
+  cout << "{ ";
+  for (auto i : result) {
+    cout << i << ", ";
+  }
+  cout << " }" << endl;
+}
+
+vector<int> selectionSort(vector<int> arr) {
+  for (int i = 0; i < arr.size(); i++) {
+    // find the max item in the remaining array and swap with its correct index
+    int maxIndex = 0;
+    int currMax = INT_MIN;
+    for (int j = 0; j < arr.size() - i; j++) {
+      if (arr[j] > currMax) {
+        currMax = arr[j];
+        maxIndex = j;
+      }
+    }
+    swap(arr[arr.size() - i - 1], arr[maxIndex]);
+  }
+
+  printVector(arr);
+  return arr;
+}
+
+// TC - O(n^2)
+// Good for sorting small data set
+// It is a unstable algorithm
+int main() {
+  vector<int> arr = {35, 65, 21, 48, 22};
+  selectionSort(arr);
+  return 0;
+}
