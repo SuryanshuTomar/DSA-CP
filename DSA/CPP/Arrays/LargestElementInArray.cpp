ab10cfb5c6f7897506ec6d48c2d1aa45df7b0306
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-11 13:27:35 +0530

    Largest Element in Array

---
+++ DSA/CPP/Arrays/LargestElementInArray.cpp
@@ -0,0 +1,20 @@
+// https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/0
+
+#include <iostream>
+#include <limits.h>
+#include <vector>
+
+class Solution {
+public:
+  //   int largest(vector<int> &arr, int n) {
+  //     int max = INT_MIN;
+  //     int i = 0;
+  //     while (i < arr.size()) {
+  //       if (arr[i] > max) {
+  //         max = arr[i];
+  //       }
+  //       i++;
+  //     }
+  //     return max;
+  //   }
+};
